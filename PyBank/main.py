8c003e5a9d470ca614b57fe62dce850b5fb3b1d1
Hi-Im-Mo <morganabbigail2014@gmail.com>
2023-09-20 18:35:09 -0500

    Updating current save before class in case of tragedy

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,13 +8,30 @@ budget_data_csv = os.path.join("Resources", "budget_data.csv")
 #don't get too excited now, cupcake. We need some variables established
 total_months = 0
 net_profit = 0
-month_changes = []
+month_values = []
 max_increase = 0
 min_increase = 0
-#current_month ?????????????????????
-#should this be in a function so i can like... establish columns??
 
 #break to scream into the void
 #OPEN THE FILE KRONK
 with open(budget_data_csv, 'r') as csvfile:
     
+    csvreader = csv.reader(csvfile, delimiter=",")
+    header = next(csvreader)
+
+    for row in csvreader:
+        if len(row)==2:
+            total_months += 1
+            net_profit += float(row[1])
+            month_values.append(row[1])
+            
+
+#print the results
+print("Financial Analysis")
+#now make it *cute*
+print(" ")
+print("---------------------")
+print(" ")
+print(f"Total Months: {total_months}")
+print(' ')
+print(f"Total: {net_profit}")
