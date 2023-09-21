6d156a2e866d7184985ed42be1364f36bf5b9b4a
Hi-Im-Mo <morganabbigail2014@gmail.com>
2023-09-20 23:13:21 -0500

    everything in PyBank done except the average

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,9 +8,14 @@ budget_data_csv = os.path.join("Resources", "budget_data.csv")
 #don't get too excited now, cupcake. We need some variables established
 total_months = 0
 net_profit = 0
-month_values = []
-max_increase = 0
-min_increase = 0
+current = 0
+last_amt = 0
+change = 0
+change_values = []
+max_increase_amount = 0
+max_increase_month = ''
+max_decrease_amount = 0
+max_decrease_month = ''
 
 #break to scream into the void
 #OPEN THE FILE KRONK
@@ -23,7 +28,19 @@ with open(budget_data_csv, 'r') as csvfile:
         if len(row)==2:
             total_months += 1
             net_profit += float(row[1])
-            month_values.append(row[1])
+            
+            current = float(row[1])
+            change = last_amt - current
+            change_values.append(change)
+            last_amt = float(row[1])
+            
+            if max_increase_amount < change:
+                max_increase_amount = change
+                max_increase_month = str(row[0])
+            if max_decrease_amount > change:
+                max_decrease_amount = change
+                max_decrease_month = str(row[0])
+        #WHY ISNT THE AVERAGE JUST THE FRICKIN AVERAGE THAT DONT MAKE SENSE
             
 
 #print the results
@@ -35,3 +52,6 @@ print(" ")
 print(f"Total Months: {total_months}")
 print(' ')
 print(f"Total: {net_profit}")
+#testy boiiis
+#print(max_decrease_amount, max_decrease_month)
+#print(max_increase_amount, max_increase_month)
